aef0188851d644d86546467c8021baaa6a247b41
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System.Windows;
+using System.Windows.Media;
+using DevExpress.Xpf.Scheduler.Drawing;
+using DevExpress.Xpf.Scheduler.Internal;
+
+namespace SchedulerRemoveTodayHighlightingWpf {
+    public partial class MainWindow : Window {
+        public MainWindow() {
+            InitializeComponent();
+        }
+    }
+
+    public class CustomDayViewCellBackgroundBrushSelector : ICellBrushSelector, ISchedulerDefaultCellBrushSelector {
+        public Brush SelectBrush(VisualResourceCellBaseContent content) {
+            VisualWorkTimeCellBaseContent workTimeCell = content as VisualWorkTimeCellBaseContent;
+            
+            if (workTimeCell == null)
+                return null;
+            if (workTimeCell.Brushes == null)
+                return null;
+
+            return workTimeCell.Brushes.CellLight;
+        }
+    }
+
+    public class CustomMonthWeekViewCellBackgroundBrushSelector : ICellBrushSelector, ISchedulerDefaultCellBrushSelector {
+        public Brush SelectBrush(VisualResourceCellBaseContent content) {
+            VisualDateCellContent dateCell = content as VisualDateCellContent;
+            
+            if (dateCell == null)
+                return null;
+            if (dateCell.Brushes == null)
+                return null;
+
+            return dateCell.Brushes.CellLight;
+        }
+    }
+
+    public class CustomTimelineViewCellBackgroundBrushSelector : ICellBrushSelector, ISchedulerDefaultCellBrushSelector {
+        public Brush SelectBrush(VisualResourceCellBaseContent content) {
+            VisualSingleTimelineCellContent cell = content as VisualSingleTimelineCellContent;
+            if (cell == null)
+                return null;
+            if (cell.Brushes == null)
+                return null;
+
+            return cell.Brushes.CellLight;
+        }
+    }
+}
---
+++ Readme.md
@@ -0,0 +1,8 @@
+# How to remove highlighting of the current day header and alternate cell color
+
+
+<p><strong>Problem:</strong></p><p></p><p>By default my SchedulerControl is displayed as follows:</p><p></p><p><img src="https://raw.githubusercontent.com/DevExpress-Examples/how-to-remove-highlighting-of-the-current-day-header-and-alternate-cell-color-e4747/12.2.10+/media/674ed945-18f2-4540-9432-77980e02da12.png"></p><p></p><p>Note that the current day header and worktime cell area are highlighted. I want to disable this highlighting for all views. </p><p></p><p><strong>Solution:</strong></p><p></p><p>These elements are rendered according to the alternate styles and background brush selectors accordingly. You can examine the default themes (see the C:\Program Files (x86)\DevExpress\DXperience 12.2\Sources\DevExpress.Xpf.Scheduler\DevExpress.Xpf.Scheduler\Themes folder) to see how this is implemented. You can override them as shown in this example. The result should be as follows:</p><p></p><p><img src="https://raw.githubusercontent.com/DevExpress-Examples/how-to-remove-highlighting-of-the-current-day-header-and-alternate-cell-color-e4747/12.2.10+/media/e514375e-a8bd-4c5c-992f-4456f2cb0097.png"></p><p></p><p><strong>See Also:</strong></p><p><a href="https://www.devexpress.com/Support/Center/p/E3450">How to customize the SchedulerControl presentation differently for different themes</a></p><p><a href="https://www.devexpress.com/Support/Center/p/E4012">How to customize the SchedulerControl presentation in the same manner for different themes</a></p>
+
+<br/>
+
+
