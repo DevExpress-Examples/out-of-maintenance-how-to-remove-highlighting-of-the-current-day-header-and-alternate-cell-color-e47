26044e5b0b4da0ec6088d17458c94b6fa965e4f7
DevExpressExampleBot <fakeEmail>
2022-06-28 16:26:20 +0400

    Source auto update [skip ci]

---
+++ VB/App.xaml.vb
@@ -0,0 +1,12 @@
+Imports System.Windows
+
+Namespace SchedulerRemoveTodayHighlightingWpf
+
+    ''' <summary>
+    ''' Interaction logic for App.xaml
+    ''' </summary>
+    Public Partial Class App
+        Inherits Application
+
+    End Class
+End Namespace
--- VB/MainWindow.xaml.vb
+++ VB/MainWindow.xaml.vb
@@ -1,61 +1,48 @@
-﻿Imports Microsoft.VisualBasic
 Imports System.Windows
 Imports System.Windows.Media
 Imports DevExpress.Xpf.Scheduler.Drawing
 Imports DevExpress.Xpf.Scheduler.Internal
 
 Namespace SchedulerRemoveTodayHighlightingWpf
-	Partial Public Class MainWindow
-		Inherits Window
-		Public Sub New()
-			InitializeComponent()
-		End Sub
-	End Class
 
-	Public Class CustomDayViewCellBackgroundBrushSelector
-		Implements ICellBrushSelector, ISchedulerDefaultCellBrushSelector
+    Public Partial Class MainWindow
+        Inherits Window
+
+        Public Sub New()
+            Me.InitializeComponent()
+        End Sub
+    End Class
+
+    Public Class CustomDayViewCellBackgroundBrushSelector
+        Implements ICellBrushSelector, ISchedulerDefaultCellBrushSelector
+
         Public Function SelectBrush(ByVal content As VisualResourceCellBaseContent) As Brush Implements ICellBrushSelector.SelectBrush
             Dim workTimeCell As VisualWorkTimeCellBaseContent = TryCast(content, VisualWorkTimeCellBaseContent)
-
-            If workTimeCell Is Nothing Then
-                Return Nothing
-            End If
-            If workTimeCell.Brushes Is Nothing Then
-                Return Nothing
-            End If
-
+            If workTimeCell Is Nothing Then Return Nothing
+            If workTimeCell.Brushes Is Nothing Then Return Nothing
             Return workTimeCell.Brushes.CellLight
         End Function
-	End Class
+    End Class
 
-	Public Class CustomMonthWeekViewCellBackgroundBrushSelector
-		Implements ICellBrushSelector, ISchedulerDefaultCellBrushSelector
+    Public Class CustomMonthWeekViewCellBackgroundBrushSelector
+        Implements ICellBrushSelector, ISchedulerDefaultCellBrushSelector
+
         Public Function SelectBrush(ByVal content As VisualResourceCellBaseContent) As Brush Implements ICellBrushSelector.SelectBrush
             Dim dateCell As VisualDateCellContent = TryCast(content, VisualDateCellContent)
-
-            If dateCell Is Nothing Then
-                Return Nothing
-            End If
-            If dateCell.Brushes Is Nothing Then
-                Return Nothing
-            End If
-
+            If dateCell Is Nothing Then Return Nothing
+            If dateCell.Brushes Is Nothing Then Return Nothing
             Return dateCell.Brushes.CellLight
         End Function
-	End Class
+    End Class
 
-	Public Class CustomTimelineViewCellBackgroundBrushSelector
-		Implements ICellBrushSelector, ISchedulerDefaultCellBrushSelector
+    Public Class CustomTimelineViewCellBackgroundBrushSelector
+        Implements ICellBrushSelector, ISchedulerDefaultCellBrushSelector
+
         Public Function SelectBrush(ByVal content As VisualResourceCellBaseContent) As Brush Implements ICellBrushSelector.SelectBrush
             Dim cell As VisualSingleTimelineCellContent = TryCast(content, VisualSingleTimelineCellContent)
-            If cell Is Nothing Then
-                Return Nothing
-            End If
-            If cell.Brushes Is Nothing Then
-                Return Nothing
-            End If
-
+            If cell Is Nothing Then Return Nothing
+            If cell.Brushes Is Nothing Then Return Nothing
             Return cell.Brushes.CellLight
         End Function
-	End Class
+    End Class
 End Namespace
